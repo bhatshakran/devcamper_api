57c9cc498fc780ae91ef12eb2b8c51308bad9e10
shaqran_bhat <bhatshakran@gmail.com>
2021-06-21 12:29:31 +0530

    Error Middleware modified

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,8 +1,8 @@
-// @desc Get all bootcamps
-// @route GET /api/v1/bootcamps
-
+const ErrorResponse = require("../utils/errorResponse");
 const Bootcamp = require("../models/Bootcamp");
 
+// @desc Get all bootcamps
+// @route GET /api/v1/bootcamps
 // @access Public
 exports.getBootcamps = async (req, res, next) => {
   try {
@@ -24,13 +24,21 @@ exports.getBootcamp = async (req, res, next) => {
     const bootcamp = await Bootcamp.findById(req.params.id);
 
     if (!bootcamp) {
-      res.status(400).json({ success: false });
+      next(
+      
+        new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
+      
+      );
     }
 
     res.status(200).json({ success: true, data: bootcamp });
   } catch (err) {
     // res.status(400).json({ success: false });
-    next(err);
+    next(
+      
+      new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
+    
+    );
   }
 };
 
