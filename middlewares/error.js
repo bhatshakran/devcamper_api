bdf83920d416c17c982c86ae30b852929b9ab1cb
shaqran_bhat <bhatshakran@gmail.com>
2021-06-21 12:48:14 +0530

    Mongoose validation error handling 2

--- middlewares/error.js
+++ middlewares/error.js
@@ -1,24 +1,36 @@
 const ErrorResponse = require("../utils/errorResponse");
 
 const errorHandler = (err, req, res, next) => {
+  let error = { ...err };
 
-    let error = {...err}
-
-    error.message = err.message;
-    console.log(error.message)
+  error.message = err.message;
+  console.log(error);
   // Log to console for dev
   console.log(err.stack.red);
 
-
-//   Mongoose bad ObjectId
-if(err.name === 'CastError'){
+  //   Mongoose bad ObjectId
+  if (err.name === "CastError") {
     const message = `Resource not found with id of ${err.value}`;
-    error = new ErrorResponse(message, 404)
+    error = new ErrorResponse(message, 404);
+  }
+
+  // Mongoose Duplicate Key
+  if (err.code === 11000) {
+    const message = `Duplicate field value entered`;
+    error = new ErrorResponse(message, 404);
+  }
+
+
+//   Mongoose Validation Error
+if(err.name ==='ValidationError'){
+    const message = Object.values(err.errors).map(val => val.message);
+    error = new ErrorResponse(message, 400)
 }
+  
 
   res.status(error.statusCode || 500).json({
     success: false,
-    error: error.message || 'Server Error',
+    error: error.message || "Server Error",
   });
 };
 
