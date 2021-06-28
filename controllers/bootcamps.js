9d3de90ef85c74aadc45eef841722a39bfa41ee7
shaqran_bhat <bhatshakran@gmail.com>
2021-06-28 15:47:21 +0530

    added course model and seeder for courses also

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -14,11 +14,10 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
   const reqQuery = { ...req.query };
 
   // Fields to exclude
-  const removeFields = ["select", "sort", 'page', 'limit'];
+  const removeFields = ["select", "sort", "page", "limit"];
 
   // Loop over removeFields and delete them from reqQuery
   removeFields.forEach((param) => delete reqQuery[param]);
-
   console.log(reqQuery);
 
   // Create Query String
@@ -53,34 +52,37 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
   const startIndex = (page - 1) * limit;
   const endIndex = page * limit;
   const total = await Bootcamp.countDocuments();
-  
-  
+
   query = query.skip(startIndex).limit(limit);
 
   // Execution query
   const bootcamps = await query;
- 
 
-// Pagination result
+  // Pagination result
   const pagination = {};
 
-  if(endIndex < total){
+  if (endIndex < total) {
     pagination.next = {
-      page : page + 1,
-      limit
-    }
+      page: page + 1,
+      limit,
+    };
   }
 
-  if(startIndex > 0){
+  if (startIndex > 0) {
     pagination.prev = {
       page: page - 1,
-      limit
-    }
+      limit,
+    };
   }
 
   res
     .status(200)
-    .json({ success: true, count: bootcamps.length, pagination, data: bootcamps });
+    .json({
+      success: true,
+      count: bootcamps.length,
+      pagination,
+      data: bootcamps,
+    });
 });
 
 // @desc Get bootcamps within a radius
