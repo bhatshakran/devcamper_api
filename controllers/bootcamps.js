c04d9dbf588e444d29a82b96ceca5ab6cf074821
shaqran_bhat <bhatshakran@gmail.com>
2021-06-28 15:18:12 +0530

    pagination added

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -8,19 +8,18 @@ const asyncHandler = require('../middlewares/async')
 // @route GET /api/v1/bootcamps
 // @access Public
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
-
   let query;
 
   // Copy req.query
-  const reqQuery = { ...req.query}
+  const reqQuery = { ...req.query };
 
   // Fields to exclude
-  const removeFields = ['select', 'sort'];
+  const removeFields = ["select", "sort", 'page', 'limit'];
 
   // Loop over removeFields and delete them from reqQuery
-  removeFields.forEach((param) => delete reqQuery[param])
+  removeFields.forEach((param) => delete reqQuery[param]);
 
-  console.log(reqQuery)
+  console.log(reqQuery);
 
   // Create Query String
   let queryStr = JSON.stringify(reqQuery);
@@ -35,26 +34,53 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
   query = Bootcamp.find(JSON.parse(queryStr));
 
   // Select Fields
-  if(req.query.select) {
-    const fields = req.query.select.split(',').join(' ');
-    query = query.select(fields)
+  if (req.query.select) {
+    const fields = req.query.select.split(",").join(" ");
+    query = query.select(fields);
   }
 
-  // Sort 
-  if(req.query.sort){
-    const sortBy = req.query.sort.split(',').join(' ');
-    query = query.sort(sortBy)
-  }else {
-    query = query.sort('-createdAt')
+  // Sort
+  if (req.query.sort) {
+    const sortBy = req.query.sort.split(",").join(" ");
+    query = query.sort(sortBy);
+  } else {
+    query = query.sort("-createdAt");
   }
+
+  // Pagination
+  const page = parseInt(req.query.page, 10) || 1;
+  const limit = parseInt(req.query.limit, 10) || 1;
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+  const total = await Bootcamp.countDocuments();
+  
+  
+  query = query.skip(startIndex).limit(limit);
 
   // Execution query
   const bootcamps = await query;
-  console.log(bootcamps)
+ 
+
+// Pagination result
+  const pagination = {};
+
+  if(endIndex < total){
+    pagination.next = {
+      page : page + 1,
+      limit
+    }
+  }
+
+  if(startIndex > 0){
+    pagination.prev = {
+      page: page - 1,
+      limit
+    }
+  }
 
   res
     .status(200)
-    .json({ success: true, count: bootcamps.length, data: bootcamps });
+    .json({ success: true, count: bootcamps.length, pagination, data: bootcamps });
 });
 
 // @desc Get bootcamps within a radius
