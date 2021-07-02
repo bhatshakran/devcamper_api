91b08c83bcfdb15b1fa55f3d05d845d852ec38bb
shaqran_bhat <bhatshakran@gmail.com>
2021-07-02 14:21:40 +0530

    Sign jwt to a user when he signs up

--- controllers/auth.js
+++ controllers/auth.js
@@ -17,5 +17,9 @@ exports.register = asyncHandler(async (req, res, next) => {
     password,
     role,
   });
-  res.status(200).json({ success: true, user });
+
+  // Create token
+  const token = user.getSignedJwtToken();
+
+  res.status(200).json({ success: true, token });
 });
