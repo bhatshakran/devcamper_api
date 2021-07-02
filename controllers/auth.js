402e5a48995965789d7ec83d100b623b19851df8
shaqran_bhat <bhatshakran@gmail.com>
2021-07-02 14:32:59 +0530

    Login a user

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,9 +4,8 @@ const asyncHandler = require("../middlewares/async");
 const User = require("../models/User");
 
 // @desc Register User
-// @route GET /api/v1/auth/register
+// @route POST /api/v1/auth/register
 // @access Public
-
 exports.register = asyncHandler(async (req, res, next) => {
   const { name, email, password, role } = req.body;
 
@@ -23,3 +22,41 @@ exports.register = asyncHandler(async (req, res, next) => {
 
   res.status(200).json({ success: true, token });
 });
+
+
+// @desc Login User
+// @route POST /api/v1/auth/login
+// @access Public
+exports.login = asyncHandler(async (req, res, next) => {
+  const {email, password} = req.body;
+
+  // Validate email and password 
+  if(!email || !password) {
+    return next(new ErrorResponse(`Please provide an email and password`, 400))
+  }
+
+
+  // Check for user
+  const user = await User.findOne({email}).select('+password');
+
+
+  if(!user){
+    return next(new ErrorResponse(`Invalid Credentials`, 401))
+  }
+
+  // Check if password matches
+  const isMatch = await user.matchPassword(password);
+
+  if(!isMatch){
+    return next(new ErrorResponse(`Invalid Credentials`, 401));
+
+  }
+
+
+  const token = user.getSignedJwtToken();
+
+  res.status(200).json({success: true, token})
+
+
+
+})
