fe3aa7656b917d5e8d0802ffbfd391ceafa42cb9
shaqran_bhat <bhatshakran@gmail.com>
2021-08-16 11:03:26 +0530

    Created review model

---
+++ models/Review.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const ReviewSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    trim: true,
+    requried: [true, "Please add a  title"],
+    maxlength: 100,
+  },
+  text: {
+    type: String,
+    requried: [true, "Please add some text"],
+  },
+  rating: {
+    type: Number,
+    min: 1,
+    max: 10,
+    required: [true, "Please add a rating"],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  bootcamp: {
+    type: mongoose.Schema.ObjectId,
+    ref: "Bootcamp",
+    required: true,
+  },
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: "User",
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("Review", ReviewSchema);
