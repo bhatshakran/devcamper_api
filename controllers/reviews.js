f18a6668b17b42deb06e5fbedf516af92ee38500
shaqran_bhat <bhatshakran@gmail.com>
2021-08-16 12:57:24 +0530

    Delete review from the database

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -74,7 +74,7 @@ exports.addReview = asyncHandler(async (req, res, next) => {
 
 
 //  @desc  Update review
-//  @route POST /api/v1/reviews/:id
+//  @route PUT /api/v1/reviews/:id
 //  @access Private
 exports.updateReview = asyncHandler(async (req, res, next) => {
   
@@ -104,9 +104,46 @@ exports.updateReview = asyncHandler(async (req, res, next) => {
   })
   
 
-  res.status(200).json({
+  res.status(201).json({
     success: true,
     data: review
+  })
+
+});
+
+
+
+//  @desc  Delete review
+//  @route DELETE /api/v1/reviews/:id
+//  @access Private
+exports.deleteReview = asyncHandler(async (req, res, next) => {
+  
+  
+
+  const review = await Review.findById(req.params.id);
+
+  if(!review){
+    return next(
+      new ErrorResponse(`No review with the id of ${req.params.id}`, 404)
+    )
+  }
+
+
+  // Make sure review belongs to user or user is admin
+  if(review.user.toString() !== req.user.id && req.user.role !== 'admin'){
+    return next(
+      new ErrorResponse(
+        `Not authorized to update review`
+      )
+    )
+  }
+
+await review.remove()
+  
+
+  res.status(200).json({
+    success: true,
+    data: {}
   })
 
 });
