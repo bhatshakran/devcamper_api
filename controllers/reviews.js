4360b0355d1c73900db72795768abe20b622e0fb
shaqran_bhat <bhatshakran@gmail.com>
2021-08-16 12:20:58 +0530

    Add a review controller added

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -43,3 +43,30 @@ exports.getReview = asyncHandler(async (req, res, next) => {
     })
   
 });
+
+
+
+//  @desc  Add review
+//  @route POST /api/v1/bootcamps/:bootcampId/reviews
+//  @access Private
+exports.addReview = asyncHandler(async (req, res, next) => {
+  
+  req.body.bootcamp = req.params.bootcampId;
+  req.body.user = req.user.id;
+
+  const bootcamp = await Bootcamp.findById(req.params.bootcampId);
+
+  if(!bootcamp){
+    return next(
+      new ErrorResponse(`No bootcamp with the id of ${req.params.id}`, 404)
+    )
+  }
+
+  const review = await Review.create(req.body);
+
+  res.status(200).json({
+    success: true,
+    data: review
+  })
+
+});
